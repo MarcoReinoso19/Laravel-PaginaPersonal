ceb6a1b686fd37f2b6ad5edd6ce21023b754be14
Marco Reinoso <marcoreinoso19@hotmail.com>
2019-09-24 20:43:29 -0500

    Menu con estilos Integrados

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,3 +1,5 @@
 <link rel="stylesheet" type="text/css" href= "{{ asset('css/estilos-footer2.css') }}">
-      <h1>2019 Marco Reinoso. All rights reserverd</h1>
-      <h4>developed by MR</h4>
+<div class="bottom-footer">
+  <h1>2019 Marco Reinoso. All rights reserverd</h1>
+  <h4>developed by MR</h4>
+</div>
--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -1,11 +1,20 @@
 <link rel="stylesheet" type="text/css" href= "{{ asset('css/estilos-menu2.css') }}">
-            <ul class="AreaTitulo">
-                <li class="Logo"><a class="scroll" href="{{ url('home') }}">MR</a></li>
-                <li class="Nombre">Marco Reinoso</li>
-            </ul>
-            <ul class="AreaMenu">
-                <li><a class="scroll" href="{{ url('work') }}">Work</a></li>
-                <li><a class="scroll" href="{{ url('biography') }}">biography</a></li>
-                <li><a class="scroll" href="{{ url('about') }}">About</a></li>
-                <li><a class="scroll" href="{{ url('contact') }}">Contact</a></li>
-            </ul>
+<div class="Menu">
+    <div class="LeftMenu">
+            <p class="Logo"><a class="Mr" href="{{ url('home') }}">MR</a></p>
+    </div>
+    <div class="MiddleMenu">
+            <p class="Nombre">Marco Reinoso</p>
+    </div>
+    <div class="RightMenu">
+      <ul class="ListaMenu">
+          <li><a class="scroll" href="{{ url('work') }}">Work</a></li>
+          <li><a class="scroll" href="{{ url('biography') }}">Biography</a></li>
+          <li><a class="scroll" href="{{ url('about') }}">About</a></li>
+          <li><a class="scroll" href="{{ url('contact') }}">Contact</a></li>
+      </ul>
+    </div>
+
+
+
+</div>
